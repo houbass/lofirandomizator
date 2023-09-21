cc41d22b62f31f2c15c76755d16aced38442682c
houbass <laube.ondrej@seznam.cz>
2023-09-21 16:49:36 +0200

    adding-alt&titles

--- src/components/Osc.js
+++ src/components/Osc.js
@@ -736,10 +736,10 @@ export default function Osc({ scaleNotes, bpm, scale, loopStatus, metronomeStatu
             }} 
             className="row center">
 
-              <img onClick={() => {playStop(bpm, newRyhtmProgression)}} className="play" style={{cursor: "pointer"}} width={30} src={playStopText}></img>
-              <img onClick={randomProgressionFun} className="random" style={{cursor: "pointer"}} width={40} src={randomImg}></img>
-              <img onClick={saveIt} className="play" style={{cursor: "pointer"}} width={40} src={likeImg}></img>
-              <a href={downloadMidiData} download><img className="random" style={{cursor: "pointer", paddingTop: "5px"}} width={30} src={saveImg}></img></a>
+              <img onClick={() => {playStop(bpm, newRyhtmProgression)}} className="play" style={{cursor: "pointer"}} width={30} src={playStopText} alt="play/stop" title="play/stop"></img>
+              <img onClick={randomProgressionFun} className="random" style={{cursor: "pointer"}} width={40} src={randomImg} alt="random" title="random"></img>
+              <img onClick={saveIt} className="play" style={{cursor: "pointer"}} width={40} src={likeImg} alt="favorite" title="favorite"></img>
+              <a href={downloadMidiData} download><img className="random" style={{cursor: "pointer", paddingTop: "5px"}} width={30} src={saveImg} alt="download midi" title="download midi"></img></a>
             </div>
           </div>
 
@@ -958,9 +958,9 @@ export default function Osc({ scaleNotes, bpm, scale, loopStatus, metronomeStatu
                 <p className="w4">{item.bpm}BPM</p>
               </div>
               <div className="row g10">
-                <img onClick={() => {setSaves(saves.filter((item2, index2) => {if(index2 === index){return}else{return item2}}))}} className="random" style={{cursor: "pointer"}} width={25} src={deleteImg}></img>
-                <a href={item.midi} download><img className="random" style={{cursor: "pointer", paddingTop: "5px"}} width={20} src={saveImg}></img></a>
-                <img onClick={() => {playStop(item.bpm, item.data)}} className="play" style={{cursor: "pointer"}} width={20} src={playStopText}></img>
+                <img onClick={() => {setSaves(saves.filter((item2, index2) => {if(index2 === index){return}else{return item2}}))}} className="random" style={{cursor: "pointer"}} width={25} src={deleteImg} alt="delete" title="delete"></img>
+                <a href={item.midi} download><img className="random" style={{cursor: "pointer", paddingTop: "5px"}} width={20} src={saveImg} alt="download midi" title="download midi"></img></a>
+                <img onClick={() => {playStop(item.bpm, item.data)}} className="play" style={{cursor: "pointer"}} width={20} src={playStopText} alt="play/stop" title="play/stop"></img>
               </div>
             </div>
           )
